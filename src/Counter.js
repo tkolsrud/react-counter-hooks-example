1b0b2d974eca93566cba4ca530e6ef5dc7ad9c9a
tkolsrud <tom.kolsrud@generalassemb.ly>
2022-04-17 13:33:51 -0700

    simple hooks example

---
+++ src/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    const handleIncrement = () => setCount(count + 1)
+    const handleDecrement = () => setCount(count - 1)
+
+    return (
+        <>
+            <span>Current Count: {count}</span>
+            <section>
+                <button onClick={handleIncrement}>+</button>
+                <button onClick={handleDecrement}>-</button>
+            </section>
+        </>
+    )
+
+}
+
+export default Counter
